09bf4ef54755bcc83942f43c7f5c21e892695032
Taylor McKenzie <taymcken@uat.edu>
2019-03-03 22:10:01 -0700

    Finished Textbook Copying
    This is everything but it isn't working properly

--- Assignment 07/Keywords II/Source.cpp
+++ Assignment 07/Keywords II/Source.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main()
 {
 	//Setup for the program
-	const int MAX_WRONG = 8; //
+	const int MAX_WRONG = 8; //How many guesses you can get wrong
 
 	vector<string> words;  //words to guess
 	words.push_back("DOCUMENT");
@@ -23,7 +23,7 @@ int main()
 
 	const string THE_WORD = words[0];
 		//words to guess
-	int wrong - 0;
+	int wrong = 0;
 		//number of incorrect guesses
 	string soFar(THE_WORD.size(), '-');
 		//word guessed so far
@@ -34,5 +34,58 @@ int main()
 	cout << "The Training Course for Deciphering will now begin." << endl;
 
 
+	//MAIN LOOPY LOOP
+	if ((wrong < MAX_WRONG) && (soFar != THE_WORD))
+	{
+		cout << "\n\nYou have " << (MAX_WRONG - wrong);
+		cout << " incorrect guesses left.\n";
+		cout << "\nYou've used the following letters:\n" << used << endl;
+		cout << "\nSo far, the word is:\n" << soFar << endl;
+	}
 
+
+	//TAKING PLAYER GUESSES
+	char guess;
+	cout << "\n\nEnter your guess: ";
+	cin >> guess;
+	guess = toupper(guess);
+	
+	if (used.find(guess) != string::npos)
+	{
+		cout << "\nYou've already guessed " << guess << endl;
+		cout << "Enter your guess: ";
+		cin >> guess;
+		guess = toupper(guess);
+	}
+
+	used += guess;
+
+	if (THE_WORD.find(guess) != string::npos)
+	{
+		cout << "That's right! " << guess << " is in the word.\n";
+			//update soFar to include the newly guessed letter
+		for (int i = 0; i < THE_WORD.length(); ++i)
+		{
+			if (THE_WORD[i] == guess)
+			{
+				soFar[i] = guess;
+			}
+		}
+	}
+
+
+	//SHUT THE GAME DOWN
+	if (wrong == MAX_WRONG)
+	{
+		cout << "\nLockdown initiated!!!" << endl;
+		cout << "You've been caught, Agent. I expected better from you." << endl;
+	}
+	else
+	{
+		cout << "\nAccess granted.  Great work, Agent." << endl;
+	}
+
+	cout << "\nThe word was " << THE_WORD << endl;
+	return 0;
+	system("pause");
 }
